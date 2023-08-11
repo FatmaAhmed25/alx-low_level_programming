da43275ac13a9dfdcd742b56123d7c4f4890c2f9
Fatma Ahmed <fatmaahmed2901@gmail.com>
2023-08-10 19:37:49 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 
 
 /**
- * main - Prints the size of various types on the computer 
- * it is compiled and run on
- * Return:Always 0.
+ * main - Prints the size of various types based on
+ * the computer it is compiled and run on. .
+ * Return: Always 0.
  */
 
 int main(void)
