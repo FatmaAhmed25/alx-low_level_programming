05be13bc7127c2050f88ace43fdc2ff829189c4b
Fatma Ahmed <fatmaahmed2901@gmail.com>
2023-08-17 19:07:27 -0700

    update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
  * @a: The first number to multiply
  * @b: The second number to multiply with first number
  *
- * Return: The result of multiplication of a and b
+ * Return: The result of multiplication betwwen a param and b param
  */
 int mul(int a, int b)
 {
