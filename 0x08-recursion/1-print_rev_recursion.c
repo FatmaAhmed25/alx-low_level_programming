8ae2bd9535bcd20c748a814c2dfbe2dc512747c4
Fatma Ahmed <fatmaahmed2901@gmail.com>
2023-08-30 18:09:39 -0700

    no.1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: char
+ *
+ * Return: string in reverse
+ * 
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
